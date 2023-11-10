b7b19cb4ef04355044277c784678c021276000d9
JJong-03 <202201652@inu.ac.kr>
2023-11-10 10:55:16 +0900

    first commit

---
+++ include/funcs.h
@@ -0,0 +1,11 @@
+#ifdef __FUNCS_H__
+#define __FUNCS_H__
+
+#include <stdio.h>
+
+void foo(int num);
+void goo();
+
+printf("this is the end\n");
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "funcs.h"
+#include<stdio.h>
+int main(){
+    foo(2023);
+    goo();
+
+    printf("First github");
+    return 0;
+}
